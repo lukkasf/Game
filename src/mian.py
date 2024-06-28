b56f73aa213d9eee6da0c040c3d53fc1930d6912
Luka Sfara <lukasfara1@gmail.com>
2024-06-28 04:42:21 -0300

    primer commit, clock y color pantalla

--- src/mian.py
+++ src/mian.py
@@ -1,1 +1,29 @@
 import pygame
+from settings import *
+
+pygame.init() # Inicia todos los modulos (audio, imagen....)
+
+SCREEN = pygame.display.set_mode(PANTALLA_RESOLUCION) # Resolucion del juego
+pygame.display.set_caption("Dark Souls") # Nombre del juego 
+
+clock = pygame.time.Clock()
+
+
+
+
+SCREEN.fill(CUSTOM) # Color de la pantalla
+
+
+contador = 0
+is_running = True
+while is_running:
+    clock.tick(60)
+    print(contador)
+    contador += 1
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+    pygame.display.flip()
+
+pygame.quit()
